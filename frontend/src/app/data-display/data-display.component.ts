1d0d4efe3ad8ec2f7410d30ff0c17105e930164c
Joel-Cooper <161271563+Joel-Cooper@users.noreply.github.com>
2025-05-13 20:20:15 +1000

    added new pipe for sorting based on dropdown values, made sure certain data fields were being stored as Numbers so sorting worked properly

--- frontend/src/app/data-display/data-display.component.ts
+++ frontend/src/app/data-display/data-display.component.ts
@@ -5,17 +5,26 @@ import { Automobile } from '../model/automobile.type';
 import { catchError } from 'rxjs';
 import { FormsModule } from '@angular/forms';
 import { FilterAutomobilesPipe } from '../pipes/filter-automobiles.pipe';
+import { SortAutomobilesPipe } from '../pipes/sort-automobiles.pipe';
 
 @Component({
   selector: 'app-data-display',
-  imports: [DataItemComponent, FormsModule, FilterAutomobilesPipe],
+  imports: [
+    DataItemComponent, 
+    FormsModule, 
+    FilterAutomobilesPipe,
+    SortAutomobilesPipe
+  ],
   templateUrl: './data-display.component.html',
   styleUrl: './data-display.component.scss'
 })
 export class DataDisplayComponent {
   automobileService = inject(AutomobilesService);
   automobileItems = signal<Array<Automobile>>([]);
+
   searchTerm = signal('');
+  sortField = signal<keyof Automobile>('name');
+  sortOrder = signal(<'ascending' | 'descending'>('ascending'));
 
   ngOnInit(): void {
     this.automobileService.getAutomobilesFromApi()
@@ -26,7 +35,18 @@ export class DataDisplayComponent {
       })
     )
     .subscribe((response) => {
-      this.automobileItems.set(response.data);
+      const cleanedData = response.data.map(item => ({
+      ...item,
+      mpg: Number(item.mpg),
+      cylinders: Number(item.cylinders),
+      displacement: Number(item.displacement),
+      horsepower: Number(item.horsepower),
+      weight: Number(item.weight),
+      acceleration: Number(item.acceleration),
+      model_year: Number(item.model_year),
+    }));
+
+      this.automobileItems.set(cleanedData);
     });
   }
 }
